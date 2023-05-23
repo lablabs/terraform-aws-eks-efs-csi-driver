da479eed436917357619e340390f6a4782cc295a
Matej Líner <matej.liner@lablabs.io>
2023-05-23 21:47:09 +0200

    fix: Updated iam policy changed in efs:csi driver 1.5.4

--- iam.tf
+++ iam.tf
@@ -35,6 +35,22 @@ data "aws_iam_policy_document" "this" {
   }
 
   statement {
+    #checkov:skip=CKV_AWS_111 there is correct condition for existing Tags
+    # Official documentation https://raw.githubusercontent.com/kubernetes-sigs/aws-efs-csi-driver/v1.3.7/docs/iam-policy-example.json
+    effect = "Allow"
+
+    actions = [
+      "elasticfilesystem:TagResource"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "aws:ResourceTag/efs.csi.aws.com/cluster"
+      values   = ["true"]
+    }
+  }
+
+  statement {
     effect = "Allow"
 
     actions = [
--- variables.tf
+++ variables.tf
@@ -50,6 +50,7 @@ variable "service_account_create" {
 }
 
 variable "service_account_name" {
+  type        = string
   default     = "aws-efs-csi-driver"
   description = "The k8s aws-efs-csi-driver service account name"
 }
@@ -121,6 +122,10 @@ variable "argo_project" {
 }
 
 variable "argo_info" {
+  type = list(object({
+    name  = string
+    value = string
+  }))
   default = [{
     "name"  = "terraform"
     "value" = "true"
@@ -129,11 +134,13 @@ variable "argo_info" {
 }
 
 variable "argo_sync_policy" {
+  type        = any
   default     = {}
   description = "ArgoCD syncPolicy manifest parameter"
 }
 
 variable "argo_metadata" {
+  type = any
   default = {
     "finalizers" : [
       "resources-finalizer.argocd.argoproj.io"
@@ -143,11 +150,13 @@ variable "argo_metadata" {
 }
 
 variable "argo_spec" {
+  type        = any
   default     = {}
   description = "ArgoCD Application spec configuration. Override or create additional spec parameters"
 }
 
 variable "argo_apiversion" {
+  type        = string
   default     = "argoproj.io/v1alpha1"
   description = "ArgoCD Appliction apiVersion"
 }
@@ -159,6 +168,7 @@ variable "argo_kubernetes_manifest_computed_fields" {
 }
 
 variable "argo_kubernetes_manifest_field_manager_name" {
+  type        = string
   default     = "Terraform"
   description = "The name of the field manager to use when applying the kubernetes manifest resource. Defaults to Terraform"
 }
